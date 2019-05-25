6cba22454bfdda3b183c20e64c913af0d61d1cc2
gordeeva-ln <43047571+gordeeva-ln@users.noreply.github.com>
2019-05-25 21:20:06 +0300

    Add comment to ls and error about wrong dir

--- CLI/src/commands/ls.py
+++ CLI/src/commands/ls.py
@@ -3,11 +3,18 @@ from src.command import ICommand
 from os import listdir, path
 
 
+'''
+List information about the FILEs (the current directory by default). Sort entries alphabetically if none of -
+'''
+
+
 class CommandLs(ICommand):
     def execute(self, pipe_input):
         if self._args:
             if len(self._args) > 1:
                 return "ls: too many arguments"
+            if not path.isdir(self._args[0]):
+                return "ls: cannot access {}: No such file or directory".format(self._args[0])
             return "\n".join(listdir(self._args[0]))
 
         return "\n".join(listdir())
