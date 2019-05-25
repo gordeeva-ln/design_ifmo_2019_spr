c01ee771abb1d8816821fa22afdcd9f353c31a11
gordeeva-ln <43047571+gordeeva-ln@users.noreply.github.com>
2019-05-25 21:21:04 +0300

    Add comment in cd, error about wrong dir

--- CLI/src/commands/cd.py
+++ CLI/src/commands/cd.py
@@ -3,11 +3,18 @@ from src.command import ICommand
 from os import chdir, path
 
 
+'''
+Change Directory - change the current working directory to a specific Folder.
+'''
+
+
 class CommandCd(ICommand):
     def execute(self, pipe_input):
         if self._args:
             if len(self._args) > 1:
                 return "cd: too many arguments"
+            if not path.isdir(self._args[0]):
+                return "cd: {}: No such file or directory".format(self._args[0])
             chdir(self._args[0])
             return ""
 
